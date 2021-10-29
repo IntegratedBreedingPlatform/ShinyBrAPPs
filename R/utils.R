35d4161b6cfd30bb54550ff2ffa32cc662375a86
leova <8135703+leova@users.noreply.github.com>
2021-10-29 10:28:22 +0200

    make_study_metadata()

--- R/utils.R
+++ R/utils.R
@@ -77,3 +77,102 @@ parse_api_url <- function(url){
     )
   )
 }
+
+#' @export
+make_study_metadata <- function(con, studyDbIds=NULL, trialDbId= NULL){
+  if(!is.null(trialDbId)){
+    ## get environment metadata by trialDbId
+    tryCatch({
+      study_metadata <- as.data.table(brapirv2::brapi_get_studies(con = con, trialDbId = trialDbId))
+    },
+    error=function(e){
+      print(e)
+      showNotification(paste0("Environment metadata not found for trialDbId ",trialDbId), type = "error", duration = notification_duration)
+    })
+  }else if(!is.null(studyDbIds)){
+    ## get environment metadata by studyDbId
+    ids <- unlist(strsplit(studyDbIds, ","))
+    study_metadata <- rbindlist(lapply(ids,function(id){
+      tryCatch({
+        as.data.table(brapirv2::brapi_get_studies(con = con, studyDbId = id))
+      },
+      error=function(e){
+        showNotification(paste0("Environment metadata not found for studyDbId ",id), type = "error", duration = notification_duration)
+      })
+    }),use.names = T, fill = T)
+    if(study_metadata[,.N]==0){
+      showNotification("No environment data found. Check apiURL, token, cropDb and studyDbIds", type = "error", duration = notification_duration)
+    }
+  }
+
+  study_ids <- unique(study_metadata$studyDbId)
+  location_ids <- unique(study_metadata$locationDbId)
+
+  ## get location abbreviations
+  withProgress(message = "Reaching location metadata", value = 0, {
+    req(location_ids)
+    loc_names <- rbindlist(l = lapply(1:length(location_ids), function(k){
+      incProgress(
+        1/length(location_ids),
+        detail = study_metadata[locationDbId==location_ids[k], unique(locationName)]
+      )
+      try({
+        brapirv2::brapi_get_locations_locationDbId(con = con, locationDbId = location_ids[k])
+      })
+    }), fill = T, use.names = T)
+  })
+  req(loc_names)
+  maxchar <- 9
+  loc_names[,location_name_abbrev := lapply(abbreviation, function(x){
+    if(is.na(x)){
+      if(nchar(locationName)>maxchar){
+        paste0(
+          substr(locationName,1,4),
+          "...",
+          substr(locationName,nchar(locationName)-3,nchar(locationName))
+        )
+      }else{
+        locationName
+      }
+    }else{
+      x
+    }
+  })]
+  study_metadata <- merge.data.table(
+    x = study_metadata[,-intersect(names(study_metadata)[names(study_metadata)!="locationDbId"], names(loc_names)), with = F],
+    # x = study_metadata[,-"locationName", with = F],
+    y = loc_names,
+    by = "locationDbId", all.x = T)
+
+  ## get/set environment number
+  if("environmentParameters.parameterName"%in%names(study_metadata)){
+    env_number <- merge.data.table(
+      x = study_metadata[,.(studyDbId = unique(studyDbId))],
+      y = study_metadata[environmentParameters.parameterName == "ENVIRONMENT_NUMBER",.(studyDbId, environment_number = environmentParameters.value)],
+      by = "studyDbId", all.x = T)
+    env_number[is.na(environment_number), environment_number:=studyDbId]
+  }else{
+    env_number <- study_metadata[,.(studyDbId = unique(studyDbId))]
+    env_number[, environment_number:=studyDbId]
+  }
+  study_metadata <- merge.data.table(
+    x = study_metadata,
+    y = env_number,
+    by = "studyDbId", all.x = T)
+
+  ## set environment names
+  study_metadata[,study_name_BMS := paste0(
+    environment_number, "-",
+    locationName
+  )]
+  study_metadata[,study_name_app := paste0(
+    study_name_BMS, " (",
+    location_name_abbrev, ")"
+  )]
+  study_metadata[,study_name_abbrev_app := paste0(
+    environment_number, "-",
+    location_name_abbrev
+  )]
+  study_metadata[, loaded:=F]
+  return(data.table(study_metadata))
+}
