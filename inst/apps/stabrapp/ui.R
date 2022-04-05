28fda7b3b10433ad5f0d6912edd5afa45b1b3387
leova <8135703+leova@users.noreply.github.com>
2022-04-05 12:40:31 +0200

    stabrapp bms-like styling

--- inst/apps/stabrapp/ui.R
+++ inst/apps/stabrapp/ui.R
@@ -3,25 +3,26 @@ source("config.R")
 
 ui <- fluidPage(
   tags$head(
-    tags$link(rel = "stylesheet", type = "text/css", href = "css/fieldbook.css"),
-    tags$link(rel = "stylesheet", type = "text/css", href = "css/style.css")
+    tags$link(rel = "stylesheet", type = "text/css", href = "css/app.css"),
+    tags$link(rel = "stylesheet", type = "text/css", href = "css/custom_styles.css")
   ),
 
   shinyjs::useShinyjs(),
   shinysky::busyIndicator(wait = 200, text = NULL, img = "img/loading-animation.gif"),
 
-  # mod_debug_ui("debug"),
-  mod_get_studydata_ui("get_studydata"),
-  tabsetPanel(id = "tabsetPanel_main",
-              # selected = "Model",
-              selected = "Data Quality",
-    tabPanel(
-      "Data Quality",
-      mod_dataquality_ui("dataquality")
-    ),
-    tabPanel(
-      "Model",
-      mod_model_ui('model')
-    )
-  ),
+  div(class = "container-fluid",
+      mod_get_studydata_ui("get_studydata"),
+      tabsetPanel(id = "tabsetPanel_main",
+                  # selected = "Model",
+                  selected = "Data Quality",
+                  tabPanel(
+                    "Data Quality",
+                    mod_dataquality_ui("dataquality")
+                  ),
+                  tabPanel(
+                    "Model",
+                    mod_model_ui('model')
+                  )
+      )
+  )
 )
