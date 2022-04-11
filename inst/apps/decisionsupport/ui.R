7d1fcb97843b11a50fa6564cb5f896745c949265
leova <8135703+leova@users.noreply.github.com>
2022-04-11 15:50:34 +0200

    decision support: BMS-like css

--- inst/apps/decisionsupport/ui.R
+++ inst/apps/decisionsupport/ui.R
@@ -3,14 +3,17 @@ source("config.R")
 
 ui <- fluidPage(
   tags$head(
-    tags$link(rel = "stylesheet", type = "text/css", href = "css/fieldbook.css"),
-    tags$link(rel = "stylesheet", type = "text/css", href = "css/style.css")
+    tags$link(rel = "stylesheet", type = "text/css", href = "css/custom_styles.css"),
+    tags$link(rel = "stylesheet", type = "text/css", href = "css/app.css")
   ),
 
   shinyjs::useShinyjs(),
   shinysky::busyIndicator(wait = 200, text = NULL, img = "img/loading-animation.gif"),
-
-  mod_get_studydata_ui("get_studydata"),
-  mod_get_extradata_ui("get_extradata"),
-  mod_scatterplot_ui("scatterplot")
+  div(class = "container-fluid",
+      tags$section(
+        mod_get_studydata_ui("get_studydata"),
+        mod_get_extradata_ui("get_extradata"),
+        mod_scatterplot_ui("scatterplot")
+      )
+  ),
 )
