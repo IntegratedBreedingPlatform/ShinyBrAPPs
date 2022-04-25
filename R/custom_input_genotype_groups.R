89054045fb55391bf5affc9b0ac86b1d385a69ec
leova <8135703+leova@users.noreply.github.com>
2022-04-25 19:46:33 +0200

    decision support: UI for selected groups: custom BS panel that includes the checkbox

--- R/custom_input_genotype_groups.R
+++ R/custom_input_genotype_groups.R
@@ -6,38 +6,62 @@
 #' @param inputId
 #' @param label
 #' @param group_table
+#' @param panel_style default, info, primary, warning, danger
 #'
 #' @export
-group_selector <- function(input_id, group_table, ...) {
+group_selector <- function(input_id, group_table, panel_style = "default", ...) {
   ns <- NS(input_id)
+  custom_panel <- div(
+    id=input_id,
+    class = "genotypes_groups",
+    tagList(
+      tags$h3("Groups of Genotypes"),
+      lapply(group_table[,group_id], function(id){
+        tagList(
+          shiny::tags$div(
+            class = paste0("panel panel-", panel_style),
+            value = paste0("value-",id),
+            shiny::tags$div(
+              class = "panel-heading",
+              role = "tab",
+              id = paste0("heading_", ns(id)),
+              tags$input(id = ns(id), type = "checkbox", class = "group_checkbox", group_id = id, value = id),
+              shiny::tags$h4(
+                class = "panel-title",
+                style = "display:inline",
+                shiny::tags$a(
+                  `data-toggle` = "collapse",
+                  href = paste0("#div-",ns(id)),
+                  group_table[group_id==id, group_name]
+                )
+              )
+            ),
+            shiny::tags$div(
+              id = paste0("div-", ns(id)),
+              class = "panel-collapse collapse",
+              role = "tabpanel",
+              shiny::tags$div(
+                class = "panel-body",
+                tags$label(paste(group_table[group_id==id, N], "genotypes: ")),
+                tags$p(group_table[group_id==id, germplasmNames_label]),
+                tags$label("Description: "),
+                tags$p(group_table[group_id==id, group_desc])
+              )
+            )
+          )
+        )
+      })
+    )
+  )
+
   shinyhtml <- tagList(
     singleton(
       tags$head(
         tags$script(src="js/custom_inputs/InputBinding_genotype_group_checkbox.js")
       )
     ),
-    div(id=input_id, class = "my_selector",
-        tagList(
-          tags$h3("Groups of Genotypes"),
-          lapply(group_table[,group_id], function(id){
-            fluidRow(
-              column(11,
-                     bsCollapsePanel(
-                       title = group_table[group_id==id, group_name],
-                       tags$label(paste(group_table[group_id==id, N], "genotypes: ")),
-                       tags$p(group_table[group_id==id, germplasmNames_label]),
-                       tags$label("Description: "),
-                       tags$p(group_table[group_id==id, group_desc])
-                       # style = "info"
-                     )
-              ),
-              column(1,
-                     tags$input(id = ns(id), type = "checkbox", class = "group_checkbox", group_id = id, value = id)
-              )
-            )
-          })
-        )
-    )
+    custom_panel
   )
+
   return(shinyhtml)
 }
--- inst/apps/decisionsupport/www/js/custom_inputs/InputBinding_genotype_group_checkbox.js
+++ inst/apps/decisionsupport/www/js/custom_inputs/InputBinding_genotype_group_checkbox.js
@@ -3,17 +3,17 @@ var binding = new Shiny.InputBinding();
 $.extend(binding, {
 
   find: function(scope) {
-    return $(scope).find(".my_selector");
+    return $(scope).find(".genotypes_groups");
   },
 
   getValue: function(el) {
-    var checkIds = [];
+    var checkedIds = [];
     $(el).find(".group_checkbox").each(function(){
       if ($(this).prop("checked") == true) {
-        checkIds.push($(this).attr("group_id"));
+        checkedIds.push($(this).attr("group_id"));
       }
     });
-    return checkIds;
+    return checkedIds;
   },
 
   subscribe: function(el, callback) {
