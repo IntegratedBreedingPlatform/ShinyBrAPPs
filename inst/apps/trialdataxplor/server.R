644c6a02cf96518c65910ce2d3f3e370c7cb1634
jframi <rami@cirad.fr>
2024-04-16 14:43:33 +0200

    Added BMS trial data explorer BrAPP

---
+++ inst/apps/trialdataxplor/server.R
@@ -0,0 +1,20 @@
+rv <- reactiveValues(
+  con = NULL,                 # brapi connection information
+  trial_metadata = NULL,      # trial metadata
+  study_metadata = NULL,      # study metadata
+  data = NULL,                # trial data as a data.table object
+  appname = "BMS Trial data explorer",
+  tr=NULL,
+  st = NULL,
+  stdatadt=NULL,
+  variables=NULL,
+  data_dq=NULL,
+  locs=NULL
+)
+
+
+server <- function(input, output, session){
+  rv <- mod_get_studydata_server("get_studydata", rv)
+  mod_banner_server("banner", rv)
+  mod_trialdataxplor_server("xplor", rv)
+}
