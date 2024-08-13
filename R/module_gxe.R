e83ada54b8b378de6bf8ca654920e46b3cae556c
jframi <rami@cirad.fr>
2024-08-13 14:47:36 +0200

    gxE UI

--- R/module_gxe.R
+++ R/module_gxe.R
@@ -5,17 +5,24 @@ mod_gxe_ui <- function(id){
     bslib::navset_tab(
       bslib::nav_panel(
         title = "Data preparation", 
-        bslib::card(
-          # bslib::card_header(
-          #   h4('Options ', icon('screwdriver-wrench'))
-          # ),
-          pickerInput(ns("picker_trait"), label = "Trait", choices = c()),
-          pickerInput(ns("picker_env"), label = "Environments", choices = c(), multiple = T),
-          pickerInput(ns("picker_location"), label = "Choose environment detail to use as location", choices = c()),
-          pickerInput(ns("picker_year"), label = "Choose environment detail to use as year", choices = c()),
-          pickerInput(ns("picker_scenario"), label = "Choose environment details to use as scenario", choices = c(), multiple = T),
-          materialSwitch(ns("check_combine_scenario"),label = "Create a full scenario column defined as the combination of selected variables", value = FALSE),
-          pickerInput(ns("picker_germplasm_attr"), label = "Select germplasm attributes to display in GxE plots", choices = c(), multiple = T)
+        bslib::layout_sidebar(
+          width = 1/3,
+          height = 800,
+          sidebar=bslib::sidebar(#bslib::card(
+            # bslib::card_header(
+            #   h4('Options ', icon('screwdriver-wrench'))
+            # ),
+            pickerInput(ns("picker_trait"), label = "Trait", choices = c()),
+            pickerInput(ns("picker_env"), label = "Environments", choices = c(), multiple = T),
+            pickerInput(ns("picker_location"), label = "Choose environment detail to use as location", choices = c()),
+            pickerInput(ns("picker_year"), label = "Choose environment detail to use as year", choices = c()),
+            pickerInput(ns("picker_scenario"), label = "Choose environment details to use as scenario", choices = c(), multiple = T),
+            materialSwitch(ns("check_combine_scenario"),label = "Create a full scenario column defined as the combination of selected variables", value = FALSE),
+            pickerInput(ns("picker_germplasm_attr"), label = "Select germplasm attributes to display in GxE plots", choices = c(), multiple = T)
+          ),#),
+          bslib::card(
+            verbatimTextOutput("TD_object_viz")
+          )
         )
       ),
       bslib::nav_panel(
@@ -99,7 +106,16 @@ mod_gxe_server <- function(id, rv){
           choices = germplasm_attr,
           selected = character(0)
         )
-        
+      })
+      
+      observe({
+        req(rv$data_plot)
+        req(rv$column_datasource)
+        req(input$picker_trait)
+        browser()
+        output$TD_object_viz <- renderPrint({
+          NULL
+        })
       })
       
     }
