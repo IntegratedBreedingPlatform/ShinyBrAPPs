f3457d2f08e1a54c8e9ffc6a5c0f515496b58ae6
jframi <rami@cirad.fr>
2024-12-12 16:06:17 +0800

    Handle download data buttons

--- R/module_banner.R
+++ R/module_banner.R
@@ -9,6 +9,7 @@ mod_banner_server <- function(id, rv){
   moduleServer(
     id,
     function(input, output, session){
+      ns <- NS(id)
       output$banner <- renderUI({
         req(rv$data)
         tags$div(
@@ -19,12 +20,18 @@ mod_banner_server <- function(id, rv){
                 margin-right:10px"),paste(rv$appname," - Study: ", paste(rv$data[,unique(trialName)], collapse = ", ")),
           style="font: 500 20px/32px Roboto,Helvetica Neue,sans-serif;display: flex;justify-content: center;position: relative; z-index: 30;",
           div(style="margin-left: auto;",
-              dropdownButton(
-                inputId = "mydropdown",
+              dropdown(
+                #style = "unite", 
+                status = "royal", width = "200px",
+                #inputId = "mydropdown",
                 label = "Download data",
-                icon = icon("fas fa-bars"),
-                circle = FALSE,
-                downloadButton(ns("dwnld1"),label = "As csv"),
+                icon = icon("fas fa-download"),
+                size="sm",
+                right=TRUE,
+                #status = "royal",
+                #circle = FALSE,
+                #downloadBttn(ns("dwnld1"),label = "As csv", style = "minimal", size="xs"),
+                downloadButton(ns("dwnld1"),label = "As csv file"),
                 downloadButton(ns("dwnld2"),label = "As TD object")
               ))),
         style = "padding: 2px 0 2px 10px;
@@ -39,6 +46,28 @@ mod_banner_server <- function(id, rv){
                     top: 0;
                     left: 3px;")
       })
+      output$dwnld1 <- downloadHandler(
+        filename = function() {
+          paste("data-", Sys.Date(), ".csv", sep="")
+        },
+        content = function(file) {
+          write.csv(rv$data_plot, file, row.names = F)
+        }
+      )
+      output$dwnld2 <- downloadHandler(
+        filename = function() {
+          paste("data-", Sys.Date(), ".Rdata", sep="")
+        },
+        content = function(file) {
+          if (is.null(rv$TD)){
+            showNotification("You must select a Trait and a Variable to use as Environment Name to form the TD object", type = "error", duration = notification_duration)
+          } else {
+            TD <- rv$TD
+            save(TD, file = file)
+          }
+        }
+      )
+      
     }
   )
 }
