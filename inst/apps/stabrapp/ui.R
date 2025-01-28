c840200626d5ecc5ef316892bccd796102045b36
Alice Boizet <alice.boizet@cirad.fr>
2025-01-28 09:07:35 +0100

    stabrapp - add an accordion_panel with excluded_observation below the 2 nav_panel modules and add the possibility to mark outliers as excluded observations

--- inst/apps/stabrapp/ui.R
+++ inst/apps/stabrapp/ui.R
@@ -58,7 +58,7 @@ ui <- fluidPage(
                     background-color: #225691;
                     margin-bottom: 5px;
                     border-color: #357ebd;"),
-  shinyjs::useShinyjs(),
+  #shinyjs::useShinyjs(),
   #shinysky::busyIndicator(wait = 200, text = NULL, img = "img/loading-animation.gif"),
   shinybusy::add_busy_spinner(spin = "fading-circle", position = "top-left", margins = c(110, 10), color = "#225691", height = 25, width =25, timeout = 200), 
   
@@ -75,6 +75,22 @@ ui <- fluidPage(
           title = "Model",
           mod_model_ui('model')
         )
-      ))
+      ),
+      bslib::accordion(
+        id = "excluded_accordion",
+        bslib::accordion_panel(
+          value = "excluded_panel",
+          title = "Excluded observations",
+          actionButton(
+            inputId = "set_non_excluded_obs",
+            label = "Include selected observations",
+            disabled = T,
+            class = "btn btn-info",
+            style = "margin-bottom: 10px;"
+          ),
+          DT::dataTableOutput("excluded_obs_table")
+        )
+      )
+    )
   )
 )
